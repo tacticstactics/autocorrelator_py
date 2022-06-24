7606fca6dd626a3415469aa793abca4f62bbc169
S <30459885+tacticstactics@users.noreply.github.com>
2022-06-24 10:52:21 +0900

    1

--- PythonApplication1/PythonApplication1/autocorrelation_interferom_def.py
+++ PythonApplication1/PythonApplication1/autocorrelation_interferom_def.py
@@ -1,6 +1,52 @@
 
+
+
+
 # autocorrelation_interferom_def.py
 
+import numpy as np
+#import matplotlib.pyplot as plt
+#import math
 
-def proc1(aaa):
-    return aaa 
+
+
+def proc1(param=0.01,m=128):
+
+
+
+
+#tcol = np.zeros((m,1)); # Electric field
+
+
+    tcol = np.zeros((m,1)); # difference
+
+    #imcol = np.zeros((m,1)) #% 
+
+    Etcol = np.zeros((m,1)); # Electric field
+
+    freq = 1
+    omega = 2*np.pi*freq
+ 
+    #print('omega=')
+    #print(omega)
+
+
+
+    for ii in range(m):
+        for jj in range(m):
+
+        #d = ii * 0.025
+
+        #print(item1)
+     
+            time1 = jj * 0.025
+            tcol[(jj)] = time1
+
+            Et = np.exp(1j * omega * time1) * (np.exp(-(time1))**2/1) + param * np.exp(1.j * omega * time1) * (np.exp(-(time1-3))**2/1)
+            #Et = math.sin(omega * time1)
+
+            Etcol[(jj)] = Et
+
+    return tcol, Etcol
+
+
--- PythonApplication1/PythonApplication1/autocorrelation_interferom_main.py
+++ PythonApplication1/PythonApplication1/autocorrelation_interferom_main.py
@@ -7,60 +7,20 @@ import math
 
 import autocorrelation_interferom_def
 
-aaa = autocorrelation_interferom_def.proc1(1)
+param = 0.01
+m = 128
 
-
+tcol, Etcol = autocorrelation_interferom_def.proc1(param,m)
 
 
 
 print('Start Main')
 
 
-m = 128
-tcol = np.zeros((m,1))   # time
-dcol = np.zeros((m,1)) # difference
-imcol = np.zeros((m,1)) #% 
-Etcol = np.zeros((m,1)); # Electric field
-
-
-freq = 1
-omega = 2*np.pi*freq
- 
-print('omega=')
-print(omega)
-
-
-
-for ii in range(m):
-  for jj in range(m):
-
-    #d = ii * 0.025
-
-     #print(item1)
-     
-    time1 = jj * 0.025
-    tcol[(jj)] = time1
-
-    Et = np.exp(1j * omega * time1) * (np.exp(-(time1))**2/1) + aaa * np.exp(1.j * omega * time1) * (np.exp(-(time1-3))**2/1)
-    #Et = math.sin(omega * time1)
-
-    Etcol[(jj)] = Et
-
-     #td = d
-     #Etd = 1
-
-     
-
-
-
-print(tcol)
-print(Etcol)
-
 plt.plot(tcol,Etcol)
 plt.show()
 
 #      print("list1=", item1, ", list2=", item2)
     
-
 
 
