ea574af618114af850a4e4dc2b6166d61c88693f
S <30459885+tacticstactics@users.noreply.github.com>
2022-09-07 10:44:05 +0900

    1

--- PythonApplication1/PythonApplication1/autocorrelation_interferom_main.py
+++ PythonApplication1/PythonApplication1/autocorrelation_interferom_main.py
@@ -39,6 +39,15 @@ ax2.plot(tcol,np.imag(Etcol))
 ax3.plot(tcol,np.angle(Etcol))
 ax4.plot(tcol,np.real(Signalcol))
 
+ax1.set_ylabel("Real Part of electric field")
+ax2.set_ylabel("Imaginary Part of electric field")
+
+ax3.set_xlabel("Delay")
+ax4.set_xlabel("Delay")
+
+ax3.set_ylabel("Phase")
+
+ax4.set_ylabel("Second Harmonic Generation Signal")
 
 #fig2 = plt.figure(figsize = (10,6), facecolor='lightblue')
 #bx1 = fig2.plot(tcol,np.real(Signalcol))
